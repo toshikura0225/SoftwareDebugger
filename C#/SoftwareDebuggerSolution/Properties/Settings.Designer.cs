f94b1e5d0e5beeaf704cbe182b9f85951e153d9e
T.Kuramata <t_kuramata@yahoo.co.jp>
2016-07-01 14:52:24 +0900

    EXEファイル名を変更

--- C#/SoftwareDebuggerSolution/Properties/Settings.Designer.cs
+++ C#/SoftwareDebuggerSolution/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SoftwareDebuggerSolution.Properties {
+namespace SoftwareDebugger.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
