2d785183283fd106971c7b30edd6d8e1925fd795
T.Kuramata <t_kuramata@yahoo.co.jp>
2016-07-01 13:26:18 +0900

    無駄な部分を削除

--- C#/SoftwareDebuggerSolution/Form1.cs
+++ C#/SoftwareDebuggerSolution/Form1.cs
@@ -14,14 +14,10 @@ namespace SoftwareDebuggerSolution
 	public partial class Form1 : Form
 	{
 		VirtualArduino arduino = new VirtualArduino();
-		VirtualTransistorIC virtualTransistor = new VirtualTransistorIC(0x20);
 
-		VirtualResistorIC virtualResistor_0 = new VirtualResistorIC(VirtualArduino.PinName.pin10);
-		VirtualResistorIC virtualResistor_1 = new VirtualResistorIC(VirtualArduino.PinName.pin8);
-
-		public MAX335<VirtualArduino.PinName> max;
-		public AD5206<VirtualArduino.PinName> ad;
-		public PCAL9555A io;
+		public MAX335 max335;
+		public AD5206 ad5206;
+		public PCAL9555A pcal9555a;
 
 		public Form1()
 		{
@@ -29,71 +25,57 @@ namespace SoftwareDebuggerSolution
 			this.comboBox1.Items.AddRange(SerialPort.GetPortNames());
 
 
-			max = new MAX335<VirtualArduino.PinName>(this.arduino.spi, this.arduino.io);
-			max.SetSwitch(new Dictionary<MAX335<VirtualArduino.PinName>.PinName, MAX335<VirtualArduino.PinName>.SwitchType>() { });
+			max335 = new MAX335(this.arduino.spi, this.arduino.io);
+			max335.SetSwitch(new Dictionary<MAX335.PinName, MAX335.SwitchType>() { });
 
-			ad = new AD5206<VirtualArduino.PinName>(this.arduino.spi, this.arduino.io);
-			ad.SetRegister(AD5206<VirtualArduino.PinName>.PinName.AW1, 125);
+			ad5206 = new AD5206(this.arduino.spi, this.arduino.io);
+			ad5206.SetRegister(AD5206.PinName.AW1, 125);
 
-			io = new PCAL9555A(this.arduino.i2c, 0x41);
-			io.SetLevel(new Dictionary<PCAL9555A.PinName, bool > () { });
+			pcal9555a = new PCAL9555A(this.arduino.i2c, 0x41);
+			pcal9555a.SetLevel(new Dictionary<PCAL9555A.PinName, bool> () { });
 
 		}
 
 
 		private void button1_Click(object sender, EventArgs e)
 		{
-			virtualTransistor.Init();
+
 		}
 
 		private void button2_Click(object sender, EventArgs e)
 		{
-			Dictionary<VirtualTransistorIC.PortType, List<bool>> directionCollection = new Dictionary<VirtualTransistorIC.PortType, List<bool>>();
-			directionCollection[VirtualTransistorIC.PortType.port0] = new List<bool>();
-			directionCollection[VirtualTransistorIC.PortType.port1] = new List<bool>();
 
-			virtualTransistor.WriteDirection(directionCollection);
 		}
 
 		private void button3_Click(object sender, EventArgs e)
 		{
-			Dictionary<VirtualTransistorIC.PortType, List<bool>> dataCollection = new Dictionary<VirtualTransistorIC.PortType, List<bool>>();
-			dataCollection[VirtualTransistorIC.PortType.port0] = new List<bool>() { true, true, true, true, true, true, true, true };
-			dataCollection[VirtualTransistorIC.PortType.port1] = new List<bool>() { false, false, false, false, false, false, false, false };
-			virtualTransistor.WriteLevel(dataCollection);
+
 		}
 
 		private void button4_Click(object sender, EventArgs e)
 		{
-			Dictionary<VirtualTransistorIC.PortType, List<bool>> dataCollection = new Dictionary<VirtualTransistorIC.PortType, List<bool>>();
-			dataCollection[VirtualTransistorIC.PortType.port0] = new List<bool>() { false, false, false, false, false, false, false, false };
-			dataCollection[VirtualTransistorIC.PortType.port1] = new List<bool>() { true, true, true, true, true, true, true, true };
-			virtualTransistor.WriteLevel(dataCollection);
+
 		}
 
 
 		private void button6_Click(object sender, EventArgs e)
 		{
-			virtualResistor_0.Init();
-			virtualResistor_1.Init();
+
 		}
 
 		private void button7_Click(object sender, EventArgs e)
 		{
-			virtualResistor_0.SetResistor(VirtualResistorIC.PinName.pin5, 100);
-			virtualResistor_1.SetResistor(VirtualResistorIC.PinName.pin5, 200);
+
 		}
 
 		private void button8_Click(object sender, EventArgs e)
 		{
-			virtualResistor_0.SetResistor(VirtualResistorIC.PinName.pin5, 200);
-			virtualResistor_1.SetResistor(VirtualResistorIC.PinName.pin5, 100);
+
 		}
 
 		private void button9_Click(object sender, EventArgs e)
 		{
-			virtualResistor_0.SetResistor(VirtualResistorIC.PinName.pin5, 255);
-			virtualResistor_1.SetResistor(VirtualResistorIC.PinName.pin5, 255);
+
 		}
 
 		private void COM_Click(object sender, EventArgs e)
@@ -104,4 +86,15 @@ namespace SoftwareDebuggerSolution
 
 
 	}
+
+	public class MAX335 : MAX335<VirtualArduino.PinName>
+	{
+		public MAX335(ISPI spi, IDigitalOutput<VirtualArduino.PinName> digitalouput) : base(spi, digitalouput) { }
+	}
+
+	public class AD5206 : AD5206<VirtualArduino.PinName>
+	{
+		public AD5206(ISPI spi, IDigitalOutput<VirtualArduino.PinName> digitalouput) : base(spi, digitalouput) { }
+	}
+	
 }
